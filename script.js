83a11f77ba9f950f1f9c82af63bb89e22b5e664a
Baldo Acerboni <baldo.acerboni@gmail.com>
2023-09-21 22:19:17 +0200

    Start adding functionalities to the sketch app
    add resizing through range input and random color generation
    now need to change mode of sketching through selection of different options

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 const canvas = document.getElementById('canvas');
-for(let i = 0; i < 100; i++) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    pixel.style.width = canvas.computedStyleMap().get('width').value / 10 + 'px';
-    pixel.style.height = canvas.computedStyleMap().get('height').value / 10 + 'px';
-    canvas.appendChild(pixel);
+const range = document.getElementById('square-nr');
+const rangeLabel = document.getElementById('range-label');
+const squares = document.getElementsByClassName('pixel');
+const selectors = document.getElementsByClassName('selector');
+const canvasWidth = 600;
+const canvasHeight = 600;
+
+
+
+function changeRange() {
+    canvas.innerHTML = '';
+    const area = range.value**2;
+    const squareWidth = canvasWidth / range.value
+    const squareHeight = canvasHeight / range.value
+    rangeLabel.textContent = `${range.value} x ${range.value}`;
+    
+    for(let i = 0; i < area; i++) {
+        const newPixel = document.createElement('div');
+        newPixel.className = 'pixel';
+        newPixel.style.width = squareWidth + 'px';
+        newPixel.style.height = squareHeight + 'px';
+        canvas.appendChild(newPixel);
+    }
+    paint()
 }
-//console.log(canvas.computedStyleMap().get('width').value)
+function paint() {
+    for(let square of squares) {
+        square.addEventListener('mouseover', color)
+    }
+    for(let selector of selectors) {
+        selector.addEventListener('click', activate)
+    }
+}
+function color(e) {
+    //WTF am I doing? go to sleep 
+
+    e.target.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+    //console.log(e.target)
+}
+function randomColor() {
+    return Math.floor(Math.random() * 256);
+}
+
+//WTF am I doing? go to sleep 
+function activate(e) {
+    e.target.style.borderColor = 'green';
+}
+changeRange();
+range.addEventListener('change', changeRange)
