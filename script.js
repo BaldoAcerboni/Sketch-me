026b36a32d60d8958d16676be256fd446c0bc5cd
Baldo Acerboni <baldo.acerboni@gmail.com>
2023-09-22 15:19:53 +0200

    Add final functionalities
    add 'darken' functionality and 'new canvas' and add last css touch on body background

--- script.js
+++ script.js
@@ -9,11 +9,14 @@ const darkerSelector = document.getElementById('darker');
 const chooseColorSelector = document.getElementById('choose');
 const eraserSelector = document.getElementById('eraser');
 const colorInput = document.getElementById('color-input');
+const cleanCanvasBtn = document.getElementById('new-canvas');
 const canvasWidth = canvas.computedStyleMap().get('width').value;
 const canvasHeight = canvas.computedStyleMap().get('height').value;
 
 function changeRange() {
+    //clear the canvas after each range modification
     canvas.innerHTML = '';
+    //do calculations here to avoid doing them inside the loop and slowing the whole thing
     const area = range.value**2;
     const squareWidth = canvasWidth / range.value
     const squareHeight = canvasHeight / range.value
@@ -26,41 +29,57 @@ function changeRange() {
         newPixel.style.height = squareHeight + 'px';
         canvas.appendChild(newPixel);
     }
-    paint()
+    for(let square of squares) {
+        square.addEventListener('mouseover', color)
+    }
 }
 
 function paint() {
     //need to put event listener(square) here, otherwise it would stop working 
     //after range modification
-    for(let square of squares) {
-        square.addEventListener('mouseover', color)
-    }
+    
     //the event listener below is only here not to make the one above feel lonely
-    //since pint() is called in changeRange() which is called in the global scope 
-    //I don't see a reason to put it outside
-    for(let selector of selectors) {
-        selector.addEventListener('click', activate)
-    }
+    //since paint() is called in changeRange() which is called in the global scope
+    //or when range is changed, I don't see a reason to put it outside
+    
 }
 
-//need to use hexadecimal values because input[type='color'].value 
-//only works on hex values and not on rgb 
+//input[type='color'].value always returns hex value, for whatever reason
+//element.style.backgroundColor always returns rgb values, because of course it does
 //(on chrome at least, did not test on other browsers)
+//I wanted to be consistent but it seems i can not
 function color(e) {
     if(blackSelector.classList.contains('active')) {
         e.target.style.backgroundColor = `#000000`;
     } else if(randomSelector.classList.contains('active')) {
         e.target.style.backgroundColor = `#${randomColor()}`;
     } else if(darkerSelector.classList.contains('active')) {
-        //MIA
+        e.target.style.backgroundColor = darkenColor(e.target)
     } else if(chooseColorSelector.classList.contains('active')) {
         e.target.style.backgroundColor = colorInput.value;
     } else if(eraserSelector.classList.contains('active')) {
         e.target.style.backgroundColor = `#ffffff`;
     }
-    
 }
-
+//extract rgb values subtract 20 from each one and return new rgb value
+function darkenColor(element) {
+    const initialColor = element.style.backgroundColor;
+    const colorValues = initialColor.slice(4, initialColor.length - 1).split(', ');
+    let darkerColor = '';
+    if(initialColor) {
+        for(let i = 0; i < colorValues.length; i++) {
+            if(colorValues[i] > 20) {
+                colorValues[i] -= 20;
+            } else {
+                colorValues[i] = 0;
+            }
+        }
+        darkerColor = `rgb(${colorValues[0]}, ${colorValues[1]}, ${colorValues[2]})`;
+    } else { //if initialColor has not yet been set initialColor === undefined
+        darkerColor = `rgb(235, 235, 235)`;
+    }
+    return darkerColor
+}
 //get random color with hexadecimal value
 function randomColor() {
     let randomHexColor = '';
@@ -69,7 +88,7 @@ function randomColor() {
     }
     return randomHexColor;
 }
-
+//deactivate all other selectors and activate selected one
 function activate(e) {
     Array.from(selectors).forEach(selector => {
         selector.classList.remove('active');    
@@ -78,7 +97,16 @@ function activate(e) {
         e.target.classList.add('active');
     }
 }
+function clearCanvas() {
+    Array.from(canvas.children).forEach(e => {
+        e.style.backgroundColor = 'rgb(255, 255, 255)';
+    })
+}
 //call function to get initial range value 
 //and apply it to both canvas and label for range-input
 changeRange();
 range.addEventListener('change', changeRange)
+for(let selector of selectors) {
+    selector.addEventListener('click', activate)
+}
+cleanCanvasBtn.addEventListener('click', clearCanvas)
