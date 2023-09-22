3b0fc1d42dd5b7cb7731ca24de8d7d487f7c5959
Baldo Acerboni <baldo.acerboni@gmail.com>
2023-09-22 11:53:16 +0200

    Add most functionalities
    squares of canvas are now colored based on selected mode.
    only missing darken mode and new canvas(clear out the sketch)

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const range = document.getElementById('square-nr');
 const rangeLabel = document.getElementById('range-label');
 const squares = document.getElementsByClassName('pixel');
 const selectors = document.getElementsByClassName('selector');
-const canvasWidth = 600;
-const canvasHeight = 600;
-
-
+const blackSelector = document.getElementById('black');
+const randomSelector = document.getElementById('random');
+const darkerSelector = document.getElementById('darker');
+const chooseColorSelector = document.getElementById('choose');
+const eraserSelector = document.getElementById('eraser');
+const colorInput = document.getElementById('color-input');
+const canvasWidth = canvas.computedStyleMap().get('width').value;
+const canvasHeight = canvas.computedStyleMap().get('height').value;
 
 function changeRange() {
     canvas.innerHTML = '';
@@ -24,27 +28,57 @@ function changeRange() {
     }
     paint()
 }
+
 function paint() {
+    //need to put event listener(square) here, otherwise it would stop working 
+    //after range modification
     for(let square of squares) {
         square.addEventListener('mouseover', color)
     }
+    //the event listener below is only here not to make the one above feel lonely
+    //since pint() is called in changeRange() which is called in the global scope 
+    //I don't see a reason to put it outside
     for(let selector of selectors) {
         selector.addEventListener('click', activate)
     }
 }
+
+//need to use hexadecimal values because input[type='color'].value 
+//only works on hex values and not on rgb 
+//(on chrome at least, did not test on other browsers)
 function color(e) {
-    //WTF am I doing? go to sleep 
-
-    e.target.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
-    //console.log(e.target)
+    if(blackSelector.classList.contains('active')) {
+        e.target.style.backgroundColor = `#000000`;
+    } else if(randomSelector.classList.contains('active')) {
+        e.target.style.backgroundColor = `#${randomColor()}`;
+    } else if(darkerSelector.classList.contains('active')) {
+        //MIA
+    } else if(chooseColorSelector.classList.contains('active')) {
+        e.target.style.backgroundColor = colorInput.value;
+    } else if(eraserSelector.classList.contains('active')) {
+        e.target.style.backgroundColor = `#ffffff`;
+    }
+    
 }
+
+//get random color with hexadecimal value
 function randomColor() {
-    return Math.floor(Math.random() * 256);
+    let randomHexColor = '';
+    for(let i = 0; i < 6; i++) {
+        randomHexColor += Math.floor(Math.random() * 17).toString(16);
+    }
+    return randomHexColor;
 }
 
-//WTF am I doing? go to sleep 
 function activate(e) {
-    e.target.style.borderColor = 'green';
+    Array.from(selectors).forEach(selector => {
+        selector.classList.remove('active');    
+    });
+    if (e.target.classList.contains('selector')) {
+        e.target.classList.add('active');
+    }
 }
+//call function to get initial range value 
+//and apply it to both canvas and label for range-input
 changeRange();
 range.addEventListener('change', changeRange)
